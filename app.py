9a8c1d2b62a63cffcd9438974b18bdc550bc681a
MTAHA826 <muhammadtaha258200@gmail.com>
2024-12-30 22:06:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
-from audio import transcribe_audio
+import audio
 from streamlit_mic_recorder import mic_recorder
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
